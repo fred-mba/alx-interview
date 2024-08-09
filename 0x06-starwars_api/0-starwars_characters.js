a64c1c8ae6c41e2da3048867ce6d83db189af8a7
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-09 05:22:59 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -13,6 +13,7 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
+
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
   request(actors[x], function (err, res, body) {
