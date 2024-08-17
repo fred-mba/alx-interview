9abea9d81fc96d3f6bb069ce6ae4e81bcbe1eff8
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-17 10:55:56 +0300

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""UTF-8 validation module
+"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding.
+       data: array of multiple integer character
+       Return True if data is a valid UTF-8 encoding, else False
+    """
+    number_bytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+    for num in data:
+        mask = 1 << 7
+        if number_bytes == 0:
+            while mask & num:
+                number_bytes += 1
+                mask >>= 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+            number_bytes -= 1
+    return number_bytes == 0
